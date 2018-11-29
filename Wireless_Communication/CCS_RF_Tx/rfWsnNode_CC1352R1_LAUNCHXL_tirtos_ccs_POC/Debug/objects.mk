5b929bf78ce19afc4deccacaf7e0b7940a35f4f9
Dhruva Koley <dhruvakoley@gmail.com>
2018-11-29 13:31:16 -0700

    Folder with modified project .c files only

--- Wireless_Communication/CCS_RF_Tx/rfWsnNode_CC1352R1_LAUNCHXL_tirtos_ccs_POC/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"/source/ti/display/lib/display.aem4f" -l"/source/ti/grlib/lib/ccs/m4f/grlib.a" -l"/source/ti/drivers/rf/lib/rf_multiMode_cc13x2_v1.aem4f" -l"/source/ti/drivers/lib/drivers_cc13x2_v1.aem4f" -l"/kernel/tirtos/packages/ti/dpl/lib/dpl_cc13x2_v1.aem4f" $(GEN_CMDS__FLAG) -l"/source/ti/devices/cc13x2_cc26x2_v1/driverlib/bin/ccs/driverlib.lib" -llibc.a
-
