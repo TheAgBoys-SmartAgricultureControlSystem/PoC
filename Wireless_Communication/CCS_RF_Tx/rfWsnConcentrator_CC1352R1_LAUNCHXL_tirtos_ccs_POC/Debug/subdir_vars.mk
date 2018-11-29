545aec737116ec3227e024496182a72960d6f72a
Dhruva Koley <dhruvakoley@gmail.com>
2018-11-29 08:46:13 -0700

    Upload POC version of CCS projects for Rx Tx

---
+++ Wireless_Communication/CCS_RF_Tx/rfWsnConcentrator_CC1352R1_LAUNCHXL_tirtos_ccs_POC/Debug/subdir_vars.mk
@@ -0,0 +1,59 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../CC1352R1_LAUNCHXL_TIRTOS.cmd 
+
+C_SRCS += \
+../CC1352R1_LAUNCHXL.c \
+../CC1352R1_LAUNCHXL_fxns.c \
+../ConcentratorRadioTask.c \
+../ConcentratorTask.c \
+../ccfg.c \
+../rfWsnConcentrator.c 
+
+C_DEPS += \
+./CC1352R1_LAUNCHXL.d \
+./CC1352R1_LAUNCHXL_fxns.d \
+./ConcentratorRadioTask.d \
+./ConcentratorTask.d \
+./ccfg.d \
+./rfWsnConcentrator.d 
+
+OBJS += \
+./CC1352R1_LAUNCHXL.obj \
+./CC1352R1_LAUNCHXL_fxns.obj \
+./ConcentratorRadioTask.obj \
+./ConcentratorTask.obj \
+./ccfg.obj \
+./rfWsnConcentrator.obj 
+
+OBJS__QUOTED += \
+"CC1352R1_LAUNCHXL.obj" \
+"CC1352R1_LAUNCHXL_fxns.obj" \
+"ConcentratorRadioTask.obj" \
+"ConcentratorTask.obj" \
+"ccfg.obj" \
+"rfWsnConcentrator.obj" 
+
+C_DEPS__QUOTED += \
+"CC1352R1_LAUNCHXL.d" \
+"CC1352R1_LAUNCHXL_fxns.d" \
+"ConcentratorRadioTask.d" \
+"ConcentratorTask.d" \
+"ccfg.d" \
+"rfWsnConcentrator.d" 
+
+C_SRCS__QUOTED += \
+"../CC1352R1_LAUNCHXL.c" \
+"../CC1352R1_LAUNCHXL_fxns.c" \
+"../ConcentratorRadioTask.c" \
+"../ConcentratorTask.c" \
+"../ccfg.c" \
+"../rfWsnConcentrator.c" 
+
+
